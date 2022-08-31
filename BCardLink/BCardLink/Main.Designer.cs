3f529102bac316def7adcafc2024067ddf534e8c
Azolukwam Nelson Nestor <nellymandela97@gmail.com>
2022-08-31 02:02:22 +0100

    Add files via upload

---
+++ BCardLink/BCardLink/Main.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace BCardLink
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboDevices = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblReaderIndicator = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLink = new System.Windows.Forms.Button();
+            this.txtRegNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUID = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.mainTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.mainTableLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboDevices
+            // 
+            this.cboDevices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevices.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cboDevices.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboDevices.FormattingEnabled = true;
+            this.cboDevices.Location = new System.Drawing.Point(73, 3);
+            this.cboDevices.Name = "cboDevices";
+            this.cboDevices.Size = new System.Drawing.Size(410, 28);
+            this.cboDevices.TabIndex = 1;
+            this.cboDevices.SelectedIndexChanged += new System.EventHandler(this.cboDevices_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblReaderIndicator
+            // 
+            this.lblReaderIndicator.BackColor = System.Drawing.Color.Red;
+            this.lblReaderIndicator.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblReaderIndicator.Location = new System.Drawing.Point(489, 10);
+            this.lblReaderIndicator.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.lblReaderIndicator.Name = "lblReaderIndicator";
+            this.lblReaderIndicator.Size = new System.Drawing.Size(14, 15);
+            this.lblReaderIndicator.TabIndex = 241;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLink);
+            this.groupBox1.Controls.Add(this.txtRegNo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtUID);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(506, 157);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // btnLink
+            // 
+            this.btnLink.Location = new System.Drawing.Point(82, 167);
+            this.btnLink.Name = "btnLink";
+            this.btnLink.Size = new System.Drawing.Size(388, 53);
+            this.btnLink.TabIndex = 2;
+            this.btnLink.Text = "LINK";
+            this.btnLink.UseVisualStyleBackColor = true;
+            this.btnLink.Visible = false;
+            // 
+            // txtRegNo
+            // 
+            this.txtRegNo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRegNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRegNo.Location = new System.Drawing.Point(82, 96);
+            this.txtRegNo.Name = "txtRegNo";
+            this.txtRegNo.Size = new System.Drawing.Size(388, 38);
+            this.txtRegNo.TabIndex = 1;
+            this.txtRegNo.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "UID:";
+            // 
+            // txtUID
+            // 
+            this.txtUID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUID.Location = new System.Drawing.Point(82, 27);
+            this.txtUID.Name = "txtUID";
+            this.txtUID.ReadOnly = true;
+            this.txtUID.Size = new System.Drawing.Size(388, 32);
+            this.txtUID.TabIndex = 1;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(3, 204);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(506, 35);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Please select device to use above";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.lblReaderIndicator, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cboDevices, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(506, 35);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // mainTableLayout
+            // 
+            this.mainTableLayout.ColumnCount = 1;
+            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTableLayout.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.mainTableLayout.Controls.Add(this.groupBox1, 0, 1);
+            this.mainTableLayout.Controls.Add(this.lblMessage, 0, 2);
+            this.mainTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTableLayout.Location = new System.Drawing.Point(0, 0);
+            this.mainTableLayout.Name = "mainTableLayout";
+            this.mainTableLayout.RowCount = 3;
+            this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.mainTableLayout.Size = new System.Drawing.Size(512, 239);
+            this.mainTableLayout.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(512, 239);
+            this.Controls.Add(this.mainTableLayout);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ID Card Reader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.mainTableLayout.ResumeLayout(false);
+            this.mainTableLayout.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboDevices;
+        private System.Windows.Forms.Label lblReaderIndicator;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUID;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnLink;
+        private System.Windows.Forms.TextBox txtRegNo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel mainTableLayout;
+    }
+}
+
---
+++ BCardLink/BCardLink/Main.cs
@@ -0,0 +1,133 @@
+﻿using MiFare;
+using MiFare.Classic;
+using MiFare.Devices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BCardLink
+{
+    public partial class Main : Form
+    {
+        private SmartCardReader reader;
+        private MiFareCard card;
+
+        public Main()
+        {
+            InitializeComponent();
+            GetDevices();
+        }
+
+        private void CardRemoved(object sender, EventArgs e)
+        {
+            card?.Dispose();
+            card = null;
+
+            var ignored = this.BeginInvoke((Action)(() =>
+            {
+                lblReaderIndicator.BackColor = Color.Red;
+                WriteMessage("Place card on the reader to scan.");
+                groupBox1.Visible = false;
+            }));
+        }
+
+        private async void CardAdded(object sender, CardEventArgs args)
+        {
+            try
+            {
+                card = args.SmartCard.CreateMiFareCard();
+
+                var cardIdentification = await card.GetCardInfo();
+                var uid = await card.GetUid();
+
+                var ignored = this.BeginInvoke((Action)(() =>
+                {
+                    lblReaderIndicator.BackColor = Color.Green;
+                    WriteMessage("Card detected. Do not remove while linking.");
+                    groupBox1.Visible = true;
+                    txtUID.Text = BitConverter.ToString(uid);
+                }));
+            }
+            catch (Exception ex)
+            {
+                PopupMessage("CardAdded Exception: " + ex.Message);
+            }
+        }
+
+        private void cboDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DeactivateDevice();
+            ConnectToDevice(cboDevices.Text);
+        }
+
+        private void GetDevices()
+        {
+            try
+            {
+
+                IReadOnlyList<string> readers = CardReader.GetReaderNames();
+
+                cboDevices.Items.AddRange(readers.ToArray());
+            }
+            catch (Exception e)
+            {
+                PopupMessage("Exception: " + e.Message);
+            }
+        }
+
+        private async void ConnectToDevice(string name)
+        {
+            try
+            {
+                reader = await CardReader.FindAsync(name);
+                if (reader == null)
+                {
+                    PopupMessage("No Readers Found");
+                    return;
+                }
+
+                reader.CardAdded += CardAdded;
+                reader.CardRemoved += CardRemoved;
+                WriteMessage("Place card on the reader to scan.");
+            }
+            catch (Exception e)
+            {
+                PopupMessage("Exception: " + e.Message);
+            }
+        }
+
+        public void PopupMessage(string message)
+        {
+            var ignored = this.BeginInvoke((Action)(() =>
+            {
+                MessageBox.Show(message);
+            }));
+        }
+
+        public void WriteMessage(string message)
+        {
+            var ignored = this.BeginInvoke((Action)(() =>
+            {
+               lblMessage.Text =  message;
+            }));
+        }
+
+        private void DeactivateDevice()
+        {
+            if (reader == null)
+            {
+                return;
+            }
+            reader.CardAdded -= CardAdded;
+            reader.CardRemoved -= CardRemoved;
+            reader.Dispose();
+            reader = null;
+        }
+    }
+}
